17fd5bb1956229891264047f5adf7e6629211985
ikvasnica <ivan@ikvasnica.com>
2016-04-11 04:17:58 +0200

    Section: Projects

--- js/site.js
+++ js/site.js
@@ -1,5 +1,16 @@
 $(function () {
 
-  $('[data-toggle="tooltip"]').tooltip()
-  
+	$("body").removeClass("no-js"); // JavaScript detection
+
+	var trialButton = $("#trial-btn");
+
+	$('[data-toggle="tooltip"]').tooltip();
+
+	$("#jumbotron-picture").delay(500).fadeIn(2000, function() {
+		$("#headings").animate({
+			opacity: 1,
+			left: "-=200px"
+		}, 2000);
+	});
+
 })
