2b7961fb6fc37363e965f3e78512b61018a7c5c8
ikvasnica <ivan@ikvasnica.com>
2016-04-13 04:15:47 +0200

    Smooth scrolling

--- js/site.js
+++ js/site.js
@@ -69,6 +69,20 @@ $(function () {
 	   throttle: 50
 	})
 
+	// smooth scrolling
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+		    $('html, body').animate({
+		      scrollTop: target.offset().top
+		    }, 1000);
+		    return false;
+		  }
+		}
+	});
+
 	// resets visibility when window resizing
 	$(window).resize(function () {
 		if (isSmallScreen()) {
